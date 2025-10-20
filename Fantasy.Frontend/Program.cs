10cd349bd28d0a25720c45abaa1aaa44a6f038a0
ccp-cyber <carloscarvajalpelaez@gmail.com>
2025-10-20 11:34:38 +1000

    Edit and delete countries

--- Fantasy.Frontend/Program.cs
+++ Fantasy.Frontend/Program.cs
@@ -1,7 +1,9 @@
+using CurrieTechnologies.Razor.SweetAlert2;
 using Fantasy.Frontend;
 using Fantasy.Frontend.Repositories;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Logging.Abstractions;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -9,5 +11,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7085") });
 builder.Services.AddScoped<IRepository, Repository>();
+builder.Services.AddLocalization();
+builder.Services.AddSweetAlert2();
 
 await builder.Build().RunAsync();
--- Fantasy.Frontend/Repositories/IRepository.cs
+++ Fantasy.Frontend/Repositories/IRepository.cs
@@ -2,6 +2,14 @@
 
 public interface IRepository
 {
+    Task<HttpResponseWrapper<object>> DeleteAsync(string url);
+
+    //este son metodos que cuando hacen algo no devuelven nada
+    Task<HttpResponseWrapper<object>> PutAsync<T>(string url, T model);
+
+    //este tipo de metodos responden algo
+    Task<HttpResponseWrapper<TActionResponse>> PutAsync<T, TActionResponse>(string url, T model);
+
     Task<HttpResponseWrapper<T>> GetAsync<T>(string url);
 
     Task<HttpResponseWrapper<object>> PostAsync<T>(string url, T model);
